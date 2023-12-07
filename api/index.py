b3575a50283822ef0a80a2fe5918ac7b0ea419a1
JabulaniSifundza <jjsifundza@gmail.com>
2023-12-07 20:58:46 +0200

    Portfolio Optimize

--- api/index.py
+++ api/index.py
@@ -4,6 +4,9 @@ import numpy as np
 import yfinance as yf
 import requests
 import json
+from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt import risk_models
+from pypfopt import expected_returns
 
 app = Flask(__name__)
 
