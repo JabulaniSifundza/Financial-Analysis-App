2caefdc61e6b8ad82d3b3b5473ae20c7813f44a6
Jabulani J Sifundza <76124269+JabulaniSifundza@users.noreply.github.com>
2023-12-07 20:18:41 +0200

    Add files via upload

---
+++ api/index.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import pandas as pd
+import numpy as np
+import google.generativeai as palm
+import yfinance as yf
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
