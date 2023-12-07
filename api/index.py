b73546a40f0dd37f9e41267acfb2c6dfa8420725
JabulaniSifundza <jjsifundza@gmail.com>
2023-12-07 20:26:07 +0200

    First Deploy

--- api/index.py
+++ api/index.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
 import pandas as pd
 import numpy as np
-import google.generativeai as palm
 import yfinance as yf
 import requests
+import json
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import MinMaxScaler
+import pandas_datareader.data as web
+import statsmodels.api as sm
+from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt import risk_models
+from pypfopt import expected_returns
+import math
 
 app = Flask(__name__)
 
