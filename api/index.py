7ceccc5ca423a296c28cd4b1b0b1df32750694d6
JabulaniSifundza <jjsifundza@gmail.com>
2023-12-08 19:38:08 +0200

    PDF File attempt

--- api/index.py
+++ api/index.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import pandas as pd
 import yfinance as yf
 import requests
 import json
 from bs4 import BeautifulSoup
 import google.generativeai as palm
+import pdfplumber
 
 app = Flask(__name__, static_folder="static", template_folder="views")
 FMP_API_KEY = os.environ.get("FMP_API_KEY")
@@ -17,3 +18,35 @@ FMP_ENDPOINT = f"https://financialmodelingprep.com/api/v3/income-statement/{symb
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/general", methods=["POST"])
+def financial_qna():
+    """
+    Use the LLM and Financial Books to answer questions
+    """
+    return "ask me something"
+
+
+@app.route("/research", methods=["POST"])
+def financial_research():
+    """
+    Use the LLM to read 10Ks and answer questions on the 10Ks
+    """
+    return "ask me something"
+
+
+@app.route("/analysis", methods=["POST"])
+def financial_analysis():
+    """
+    Use Pandas and FMP API to analyse stock returns, momentum, volatility and financial statements.
+    """
+    return "ask me something"
+
+
+@app.route("/portfolio", methods=["POST"])
+def financial_portfolio():
+    """
+    Portfolio Optimization using Pandas
+    """
+    return "ask me something"
