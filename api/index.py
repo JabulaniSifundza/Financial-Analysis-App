933c61dc6d309792d061508fe904d2d4d221b9b1
JabulaniSifundza <jjsifundza@gmail.com>
2023-12-08 18:22:35 +0200

    Secrets

--- api/index.py
+++ api/index.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 import pandas as pd
 import yfinance as yf
@@ -6,6 +7,8 @@ import json
 import google.generativeai as palm
 
 app = Flask(__name__, static_folder="static", template_folder="views")
+FMP_API_KEY = os.environ.get("FMP_API_KEY")
+GOOGLE_PALM_API_KEY = os.environ.get("google_palm_api_key")
 
 
 @app.route("/")
