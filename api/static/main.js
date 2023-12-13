020e74e165e8a4d8c07d38604ce8d4eaff550e4b
JabulaniSifundza <jjsifundza@gmail.com>
2023-12-13 19:43:33 +0200

    API test

--- api/static/main.js
+++ api/static/main.js
@@ -1,15 +1,17 @@
-const general_prompt = document.getElementById("general-prompt").ariaValueMax;
-let server = ""
-let data = {
-  prompt: general_prompt
-}
-fetch(server, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(data)
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch((error)=> console.error('Error: ', error))
+document.getElementById("send-prompt").addEventListener("click", () => {
+  const general_prompt = document.getElementById("general-prompt").ariaValueMax;
+  let server = "https://financial-analysis-app.vercel.app/general";
+  let data = {
+    prompt: general_prompt,
+  };
+  fetch(server, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error: ", error));
+});
